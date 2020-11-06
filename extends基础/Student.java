514e311b97d2069b0387c318ed9aa95b47d6e3d9
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-11-06 19:34:21 +0800

    Create Student.java

---
+++ extends基础/Student.java
@@ -0,0 +1,13 @@
+import java.io.*;
+final public class Student extends People
+{
+    public int m;
+    public Student()
+    {
+        m = 1;
+    }
+    public void eat()
+    {
+        System.out.println("EAT THIS!");
+    }
+}
