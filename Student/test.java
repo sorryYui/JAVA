351621f5d7d9e511f4490a7c822d6207239de05f
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-11-06 17:57:50 +0800

    Create test.java

---
+++ Student/test.java
@@ -0,0 +1,10 @@
+public class Test
+{
+    public static void main(String args[])
+    {
+        Student s1 = new Student("s1");
+        Student s2 = new Student("s2");
+        System.out.println(s1.getCount());
+        System.out.println(s2.getCount());
+    }
+}
