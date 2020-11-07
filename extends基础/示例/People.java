1e17807b1fb0818c760a287278fa7fc42b90b50d
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-11-07 15:42:34 +0800

    Create People.java

---
+++ extends基础/示例/People.java
@@ -0,0 +1,14 @@
+  
+import java.io.*;
+public class People
+{
+    public int m;
+    public People()
+    {
+        m = 0;
+    }
+    public void eat()
+    {
+        System.out.println("Eat this!");
+    }
+}
