9e3ec895cd45a1bc798de3229b6debda966afa0d
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-11-08 15:28:09 +0800

    Create Test.java

---
+++ interface/示例/Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+    public static void main(String args[])
+    {
+        Rectangle r1 = new Rectangle(1,2);
+        Rectangle r2 = new Rectangle(3,4);
+        System.out.println(r1.compare(r2));
+    }
+}
