9df0bb2f5ae35411b45f292613cab8fb96d9e4c0
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-11-12 10:39:42 +0800

    Create GenericTest.java

---
+++ 泛型/泛型通配符/示例/GenericTest.java
@@ -0,0 +1,8 @@
+public class GenericTest
+{
+    public static void main(String args[])
+    {
+        Generic<String> genericString = new Generic("HHH");
+        genericString.showKey(genericString);
+    }
+}
