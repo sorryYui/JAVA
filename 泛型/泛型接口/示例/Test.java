47c33457b85998f4212cb32e360dbc85a80d2a66
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-11-12 09:15:40 +0800

    Create Test.java

---
+++ 泛型/泛型接口/示例/Test.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Test
+{
+    public static void main(String args[])
+    {
+        GenericString genericString = new GenericString();
+        System.out.println(genericString.next());
+    }
+}
