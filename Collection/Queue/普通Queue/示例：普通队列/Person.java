0de7eb9a50b30d7aa7c1d5971fbb8ecbc525e31a
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-11-18 10:59:18 +0800

    Create Person.java

---
+++ Collection/Queue/普通Queue/示例：普通队列/Person.java
@@ -0,0 +1,18 @@
+class Person
+{
+    public Person(String name,int sroce)
+    {
+        this.name = name;
+        this.sroce = sroce;
+    }
+    public int getSroce()
+    {
+        return sroce;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    private int sroce;
+    private String name;
+}
