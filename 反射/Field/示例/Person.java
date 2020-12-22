aa6430cc96dbc2635513d0994ef72444bf84c4db
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-12-22 20:37:10 +0800

    Create Person.java

---
+++ 反射/Field/示例/Person.java
@@ -0,0 +1,16 @@
+public class Person
+{
+    public Person()
+    {
+
+    }
+    public String name;
+    protected int age;
+    char sex;
+    private String ip;
+    public String toString()
+    {
+        return "name:" + name + " age:" + age
+                +" sex:" + sex + " ip:" + ip;
+    }
+}
