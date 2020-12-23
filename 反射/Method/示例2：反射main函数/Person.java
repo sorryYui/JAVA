6f6bdffefab3be4a027be05b51b7971d927977ae
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-12-23 10:27:46 +0800

    Update Person.java

--- 反射/Method/示例2：反射main函数/Person.java
+++ 反射/Method/示例2：反射main函数/Person.java
@@ -1,14 +1,7 @@
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-public class MethodTest
+public class Person
 {
     public static void main(String args[])
-            throws ClassNotFoundException, NoSuchMethodException,
-            InvocationTargetException, IllegalAccessException
     {
-        Class clazz = Class.forName("Person");
-        Method method = clazz.getMethod("main",String[].class);
-        method.invoke(null,(Object)(new String[]{"a","b","c"}));
+        System.out.println("main is called.");
     }
 }
