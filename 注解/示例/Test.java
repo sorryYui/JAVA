439f79e1041f6be958f3393e8bdc6ea321b5cef2
sorryYui <74042501+sorryYui@users.noreply.github.com>
2021-01-15 10:59:39 +0800

    Update Test.java

--- 注解/示例/Test.java
+++ 注解/示例/Test.java
@@ -1,14 +1,8 @@
-import java.lang.annotation.Annotation;
-@Test(age = 0,name = "HHH")
-public class Demo
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test
 {
-    public static void main(String args[])
-    {
-        Class clazz = Demo.class;
-        boolean hasAnnotation = clazz.isAnnotationPresent(Test.class);
-        if (hasAnnotation) {
-            Test annotation = (Test) clazz.getAnnotation(Test.class);
-            System.out.println(annotation.age());
-        }
-    }
+    int age() default 0;
+    String name() default "";
 }
